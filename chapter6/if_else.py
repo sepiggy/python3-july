cc2a3a289fa384dc242c8bf124e376a5ab5b7ef0
sepiggy <jms1209@qq.com>
2017-10-21 10:44:23 +0800

    chapter6 ok

---
+++ chapter6/if_else.py
@@ -0,0 +1,10 @@
+# mood = True
+mood = False
+
+# if 后面接 bool 类型变量
+if mood:
+    print('go to left')
+    # print('back away')
+# print('back away')
+else:
+    print('go to right')
---
+++ chapter6/if_else_5.py
@@ -0,0 +1,13 @@
+# a, b 两个变量, 一个为 True, 另一个为 False, 返回是 True 的那个变量
+
+a = 1
+b = 0
+
+# 使用 if-else
+if a == True:
+    print(a)
+else:
+    print(b)
+
+# 使用 or 运算符 (better)
+print(a or b)
